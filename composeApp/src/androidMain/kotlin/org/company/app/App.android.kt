672869ae56179cead7d842b45d772d0f1e7ea1f2
NotTheRobot <linglight01@gmail.com>
2024-02-20 17:38:02 +0500

    Initial commit

---
+++ composeApp/src/androidMain/kotlin/org/company/app/App.android.kt
@@ -0,0 +1,42 @@
+package org.company.app
+
+import android.app.Application
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainView() = App()
+
+class AppActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainView()
+        }
+    }
+}
+
+class AndroidApp : Application() {
+    companion object {
+        lateinit var INSTANCE: AndroidApp
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        INSTANCE = this
+    }
+}
+
+internal actual fun openUrl(url: String?) {
+    val uri = url?.let { Uri.parse(it) } ?: return
+    val intent = Intent().apply {
+        action = Intent.ACTION_VIEW
+        data = uri
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    }
+    AndroidApp.INSTANCE.startActivity(intent)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "FirstMultiplatfporm"
+include(":composeApp")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
